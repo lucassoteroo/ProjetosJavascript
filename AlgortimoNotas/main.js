e26f0e9a1f5258d49282aac82ac6c13ff9939be0
lucassoteroo <lucassoteroo14@gmail.com>
2021-08-11 21:45:03 -0300

    atualizando AlgortimoNotas

--- AlgortimoNotas/main.js
+++ AlgortimoNotas/main.js
@@ -1,60 +1,21 @@
-/*Variavel dos alunos*/
-
-var alunos = [
-
-    /*Variavel dos notasAluno*/
-
-    [7, 8, 8, 8],
-    [10, 9, 9, 7],
-    [5, 7, 7, 6],
-    [8, 8, 8, 8]
-]
-
-/*Variavel da nota*/
-
-var nota = 0;
-
-/*Laço para percorrer a array alunos*/
-
-for (var i = 0; i < alunos.length; i++) {
-    nota = 0
-    notasAluno = alunos[i]
-    console.log("Aluno: " + i);
-    console.log("Notas: " + notasAluno);
-
-    /*Laço para percorrer a array notasAluno*/
-
-    for (var c = 0; c < notasAluno.length; c++) {
-        nota += notasAluno[c];
+function calcularMedia(notas) {
+    var soma = 0;
+    for (c = 0; c < notas.length; c++) {
+        soma += notas[c];
     }
-    media = nota / 4;
+    media = soma / notas.length;
+    return media;
+}
 
-    /*Estrutura condicional para validação da média*/
-
+function aprovacao(notas) {
+    let media = calcularMedia(notas);
+    let resultado = 0;
     if (media >= 7) {
         resultado = "aprovado";
     } else {
         resultado = "reprovado";
     }
-    console.log("Media: " + media + " - " + resultado);
+    return "A média é " + media + " - O resultado é " + resultado;
 }
 
-/*Resultado:
-
-    "Aluno: 0"
-    "Notas: 7,8,8,8"
-    "Media: 7.75 - aprovado"
-    
-    "Aluno: 1"
-    "Notas: 10,9,9,7"
-    "Media: 8.75 - aprovado"
-    
-    "Aluno: 2"
-    "Notas: 5,7,7,6"
-    "Media: 6.25 - reprovado"
-    
-    "Aluno: 3"
-    "Notas: 8,8,8,8"
-    "Media: 8 - aprovado"
-
-*/
+console.log(aprovacao([8, 6, 9]));
